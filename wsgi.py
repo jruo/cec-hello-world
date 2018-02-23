f0e0ec9a7a1a9d5567a23ca644bddb8c7ea24e5b
Janne Ruoho <jruo@users.noreply.github.com>
2018-02-23 17:55:29 +0200

    Added hostname+timestamp writing to log file.

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,14 @@
 import socket
 from flask import Flask
+from datetime import datetime
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
+    log_file = open("/mnt/hello-world-storage/log", "a")
+    log_file.write(socket.gethostname() + " " + str(datetime.now()) + "\n")
+    log_file.close()
     return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
 
