47df222fda60dfcdbd2bfa35706707df06e90382
Janne Ruoho <jruo@users.noreply.github.com>
2018-02-23 17:58:39 +0200

    Added log file reading.

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,12 @@ def hello():
     log_file = open("/mnt/hello-world-storage/log", "a")
     log_file.write(socket.gethostname() + " " + str(datetime.now()) + "\n")
     log_file.close()
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    
+    log_file = open("/mnt/hello-world-storage/log", "r")
+    log_content = log_file.read()
+    log_file.close()
+    
+    return log_content
 
 
 if __name__ == "__main__":
